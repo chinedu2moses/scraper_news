b79695a614015b88732c0a47653788d608818307
chinedu moses <chinedu2moses@gmail.com>
2020-06-23 12:30:54 -0500

    env file

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,15 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
-var CommentsSchema = new Schema({
-    title: {
-        type: String,
-        
-    },
-    body: {
-        type: String,
-        required: true
-    },
 
+var CommentSchema = new Schema({
+  name: {
+    type: String
+  },
+  body: {
+    type: String,
+    required: true
+  }
 });
 var Comment = mongoose.model("Comment", CommentSchema);
 module.exports = Comment;
--- server.js
+++ server.js
@@ -1,33 +1,49 @@
+  
+//dependencies
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 
-var express = require ("express");
+//initialize Express app
+var express = require("express");
 var app = express();
 
 app.use(logger("dev"));
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 
 app.use(express.static(process.cwd() + "/public"));
-
+//Require set up handlebars
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scrapped_news");
-var db = mongoose.connection;
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+ // process.env.MONGODB_URI || "mongodb://chinedu:Nissi2013@ds013456.mlab.com:13456/heroku_ltqn5fxl";
+  process.env.MONGODB_URI || "mongodb://chine-news:Nissi2013@ds013456.mlab.com:13456/heroku_ltqn5fxl";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function(){
-    console.log("Connected to Mongoose!")
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
 });
 
+var routes = require("./controller/controller.js");
+app.use("/", routes);
 
-var port = process.env.PORT || 3000;
+//Create localhost port
+var port = process.env.PORT || 7000;
 app.listen(port, function() {
-    console.log("listening on PORT " + port);
+  console.log("Listening on PORT " + port);
 });
